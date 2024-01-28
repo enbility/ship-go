cd1e4902ca3054c2ec2d410e6bec6b3891e24e52
Andreas Linde <mail@andreaslinde.de>
2024-01-28 20:57:11 +0100

    Manully assign ship state values
    This is only done so it is easire to find the appropriate state from log values

--- model/types.go
+++ model/types.go
@@ -7,59 +7,60 @@ type ShipState struct {
 
 type ShipMessageExchangeState uint
 
+// set the values manually instead of using iota, so log data can be associated easier
 const (
 	// Connection Mode Initialisation (CMI) SHIP 13.4.3
-	CmiStateInitStart ShipMessageExchangeState = iota
-	CmiStateClientSend
-	CmiStateClientWait
-	CmiStateClientEvaluate
-	CmiStateServerWait
-	CmiStateServerEvaluate
+	CmiStateInitStart      ShipMessageExchangeState = 0
+	CmiStateClientSend     ShipMessageExchangeState = 1
+	CmiStateClientWait     ShipMessageExchangeState = 2
+	CmiStateClientEvaluate ShipMessageExchangeState = 3
+	CmiStateServerWait     ShipMessageExchangeState = 4
+	CmiStateServerEvaluate ShipMessageExchangeState = 5
 	// Connection Data Preparation SHIP 13.4.4
-	SmeHelloState
-	SmeHelloStateReadyInit
-	SmeHelloStateReadyListen
-	SmeHelloStateReadyTimeout
-	SmeHelloStatePendingInit
-	SmeHelloStatePendingListen
-	SmeHelloStatePendingTimeout
-	SmeHelloStateOk
-	SmeHelloStateAbort           // Sent abort to remote
-	SmeHelloStateAbortDone       // Sending abort to remote is done
-	SmeHelloStateRemoteAbortDone // Received abort from remote
-	SmeHelloStateRejected        // Connection closed after remote pending: "4452: Node rejected by application"
+	SmeHelloState                ShipMessageExchangeState = 6
+	SmeHelloStateReadyInit       ShipMessageExchangeState = 7
+	SmeHelloStateReadyListen     ShipMessageExchangeState = 8
+	SmeHelloStateReadyTimeout    ShipMessageExchangeState = 9
+	SmeHelloStatePendingInit     ShipMessageExchangeState = 10
+	SmeHelloStatePendingListen   ShipMessageExchangeState = 11
+	SmeHelloStatePendingTimeout  ShipMessageExchangeState = 12
+	SmeHelloStateOk              ShipMessageExchangeState = 13
+	SmeHelloStateAbort           ShipMessageExchangeState = 14 // Sent abort to remote
+	SmeHelloStateAbortDone       ShipMessageExchangeState = 15 // Sending abort to remote is done
+	SmeHelloStateRemoteAbortDone ShipMessageExchangeState = 16 // Received abort from remote
+	SmeHelloStateRejected        ShipMessageExchangeState = 17 // Connection closed after remote pending: "4452: Node rejected by application"
 
 	// Connection State Protocol Handhsake SHIP 13.4.4.2
-	SmeProtHStateServerInit
-	SmeProtHStateClientInit
-	SmeProtHStateServerListenProposal
-	SmeProtHStateServerListenConfirm
-	SmeProtHStateClientListenChoice
-	SmeProtHStateTimeout
-	SmeProtHStateClientOk
-	SmeProtHStateServerOk
+	SmeProtHStateServerInit           ShipMessageExchangeState = 18
+	SmeProtHStateClientInit           ShipMessageExchangeState = 19
+	SmeProtHStateServerListenProposal ShipMessageExchangeState = 20
+	SmeProtHStateServerListenConfirm  ShipMessageExchangeState = 21
+	SmeProtHStateClientListenChoice   ShipMessageExchangeState = 22
+	SmeProtHStateTimeout              ShipMessageExchangeState = 23
+	SmeProtHStateClientOk             ShipMessageExchangeState = 24
+	SmeProtHStateServerOk             ShipMessageExchangeState = 25
 	// Connection PIN State 13.4.5
-	SmePinStateCheckInit
-	SmePinStateCheckListen
-	SmePinStateCheckError
-	SmePinStateCheckBusyInit
-	SmePinStateCheckBusyWait
-	SmePinStateCheckOk
-	SmePinStateAskInit
-	SmePinStateAskProcess
-	SmePinStateAskRestricted
-	SmePinStateAskOk
+	SmePinStateCheckInit     ShipMessageExchangeState = 26
+	SmePinStateCheckListen   ShipMessageExchangeState = 27
+	SmePinStateCheckError    ShipMessageExchangeState = 28
+	SmePinStateCheckBusyInit ShipMessageExchangeState = 29
+	SmePinStateCheckBusyWait ShipMessageExchangeState = 30
+	SmePinStateCheckOk       ShipMessageExchangeState = 31
+	SmePinStateAskInit       ShipMessageExchangeState = 32
+	SmePinStateAskProcess    ShipMessageExchangeState = 33
+	SmePinStateAskRestricted ShipMessageExchangeState = 34
+	SmePinStateAskOk         ShipMessageExchangeState = 35
 	// ConnectionAccess Methods Identification 13.4.6
-	SmeAccessMethodsRequest
+	SmeAccessMethodsRequest ShipMessageExchangeState = 36
 
 	// Handshake approved on both ends
-	SmeStateApproved
+	SmeStateApproved ShipMessageExchangeState = 37
 
 	// Handshake process is successfully completed
-	SmeStateComplete
+	SmeStateComplete ShipMessageExchangeState = 38
 
 	// Handshake ended with an error
-	SmeStateError
+	SmeStateError ShipMessageExchangeState = 39
 )
 
 var ShipInit []byte = []byte{MsgTypeInit, 0x00}
