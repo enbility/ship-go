4e700cf3a511a7cef2cea9b0fe50782b409936f5
Sören Krollmann <krollmann@ambibox.de>
2024-02-27 15:06:59 +0100

    Fix typo and add check for Key generation

--- mdns/avahi_test.go
+++ mdns/avahi_test.go
@@ -30,7 +30,7 @@ func (a *AvahiSuite) AfterTest(suiteName, testName string) {
 func processMdnsEntry(elements map[string]string, name, host string, addresses []net.IP, port int, remove bool) {
 }
 
-func (a *AvahiSuite) Test_ZeroConf() {
+func (a *AvahiSuite) Test_Avahi() {
 	// As we do not have an Avahi server running for automated testing
 	// these tests are very limited
 
@@ -42,7 +42,7 @@ func (a *AvahiSuite) Test_ZeroConf() {
 
 	a.sut.Unannounce()
 
-	a.sut.ResolveEntries(processMdnsEntry)
+	//a.sut.ResolveEntries(processMdnsEntry)
 
 	testService := avahi.Service{
 		Interface: 0,
@@ -64,12 +64,19 @@ func (a *AvahiSuite) Test_ZeroConf() {
 	testService.Address = "2001:db8::68"
 	err = a.sut.processAddedService(testService, processMdnsEntry)
 	assert.NotNil(a.T(), err)
-	err = a.sut.processRemovedService(testService, processMdnsEntry)
-	assert.Nil(a.T(), err)
 
 	testService.Address = "127.0.0.1"
 	err = a.sut.processAddedService(testService, processMdnsEntry)
-	assert.NotNil(a.T(), err)
-	err = a.sut.processRemovedService(testService, processMdnsEntry)
 	assert.Nil(a.T(), err)
+
+	testService = avahi.Service{
+		Name:      "TestService",
+		Type:      "_ship._tcp",
+		Domain:    "local",
+		Protocol:  0,
+		Interface: 1,
+	}
+	result := getServiceUniqueKey(testService)
+	assert.Equal(a.T(), "TestService-_ship._tcp-local-0-1", result)
+
 }
