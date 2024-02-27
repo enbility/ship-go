27556b00f9f20f792b1cb66b4aad180b2375c33c
Sören Krollmann <krollmann@ambibox.de>
2024-02-27 16:15:58 +0100

    Also test if elements get stored under the SKI

--- mdns/avahi_test.go
+++ mdns/avahi_test.go
@@ -53,7 +53,6 @@ func (a *AvahiSuite) Test_Avahi() {
 	testService := avahi.Service{
 		Interface: 0,
 	}
-
 	err := a.sut.processService(testService, false, processMdnsEntry)
 	assert.NotNil(a.T(), err)
 
@@ -62,6 +61,8 @@ func (a *AvahiSuite) Test_Avahi() {
 	}
 	err = a.sut.processService(testService, false, processMdnsEntry)
 	assert.NotNil(a.T(), err)
+	err = a.sut.processService(testService, true, processMdnsEntry)
+	assert.Nil(a.T(), err)
 
 	err = a.sut.processAddedService(testService, processMdnsEntry)
 	assert.NotNil(a.T(), err)
@@ -82,8 +83,17 @@ func (a *AvahiSuite) Test_Avahi() {
 		Domain:    "local",
 		Protocol:  0,
 		Interface: 1,
+		Address:   "127.0.0.1",
+		Txt:       [][]byte{[]byte("ski=133742247331")},
 	}
 	result := getServiceUniqueKey(testService)
 	assert.Equal(a.T(), "TestService-_ship._tcp-local-0-1", result)
 
+	err = a.sut.processAddedService(testService, processMdnsEntry)
+	assert.Nil(a.T(), err)
+	assert.Equal(a.T(), "133742247331", a.sut.serviceElements["TestService-_ship._tcp-local-0-1"]["ski"])
+
+	a.sut.ifaceIndexes = []int32{avahi.InterfaceUnspec}
+	err = a.sut.processService(testService, false, processMdnsEntry)
+	assert.NotNil(a.T(), err)
 }
