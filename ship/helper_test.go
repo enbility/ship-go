93a57ec73d0a9df59ffcb8e1817334ba79a03660
Neverminda <t.h.schmitt@web.de>
2024-08-17 22:16:52 +0200

    add comments

--- ship/helper_test.go
+++ ship/helper_test.go
@@ -19,6 +19,7 @@ func TestJsonFromEEBUSJson(t *testing.T) {
 	}
 }
 
+// The PMCP device mistakenly adds an `0x00` byte at the end of many messages. Test if this is handled correctly
 func TestJsonFromEEBUSJsonTrailingZeros(t *testing.T) {
 
 	bytes := []byte(`{"datagram":[{"header":[{"specificationVersion":"1.2.0"},{"addressSource":[{"device":"d:_i:3210_EVSE"},{"entity":[1,1]},{"feature":6}]},{"addressDestination":[{"device":"d:_i:3210_HEMS"},{"entity":[1]},{"feature":1}]},{"msgCounter":194},{"msgCounterReference":4890},{"cmdClassifier":"reply"}]},{"payload":[{"cmd":[[{"deviceClassificationManufacturerData":[{"deviceName":""},{"deviceCode":""},{"brandName":""},{"powerSource":"mains3Phase"}]}]]}]}]}`)
